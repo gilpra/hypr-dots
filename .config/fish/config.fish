e57328b5ab54a77fa7f971b5daaa2d9440053d1c
Gilang Arya Pradana <gilangarya272001@gmail.com>
2026-06-30 04:45:53 +0700

    feat(shell): change shell to fish along with config

---
+++ .config/fish/config.fish
@@ -0,0 +1,55 @@
+# Jika sesi login dan berada di tty1
+if status is-login
+    if test -z "$DISPLAY" && test (tty) = /dev/tty1
+        exec Hyprland
+    end
+end
+
+###########
+## ALIAS ##
+###########
+
+alias c=clear
+alias mi=micro
+alias start='sudo systemctl start '
+alias stop='sudo systemctl stop '
+alias ff=fastfetch
+alias fishconfig='micro ~/.config/fish/config.fish'
+
+# Paket manajemen
+alias paccek='yay -Q | grep '
+alias upgrade='yay -Syu && flatpak upgrade'
+
+# Git
+alias gi='git init'
+alias gs='git status'
+alias ga='git add .'
+alias gcm='git commit -m '
+alias gp='git push'
+alias gc='git clone '
+alias gf='git fetch'
+alias grh='git reset --hard '
+alias grr='git remote remove '
+
+# Perintah sistem
+alias grubup="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias fixpacman="sudo rm /var/lib/pacman/db.lck"
+alias hw='hwinfo --short' # Informasi hardware
+alias update='sudo pacman -Syu'
+alias mirror="sudo cachyos-rate-mirrors"
+alias ls='eza -al --color=always --group-directories-first --icons' # Pengganti ls dengan eza
+
+# Membersihkan orphaned packages
+function cleanup
+    set orphaned (pacman -Qtdq)
+    if test -n "$orphaned"
+        sudo pacman -Rns $orphaned
+    else
+        echo "There are no packages to clean."
+    end
+end
+
+# Mendapatkan error dari journalctl
+alias jctl="journalctl -p 3 -xb"
+alias clrjctl="sudo journalctl --vacuum-time=1s"
+
